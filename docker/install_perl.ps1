10b15bab32c973aacd16808c0230b36d675981a2
Andres Freund <andres@anarazel.de>
2021-12-14 10:27:23 -0800

    fix & perl

---
+++ docker/install_perl.ps1
@@ -0,0 +1,10 @@
+mkdir c:\t ;
+cd c:\t ;
+
+echo 'installing perl';
+curl.exe -sSL -o perl.zip `
+    https://strawberryperl.com/download/5.26.3.1/strawberry-perl-5.26.3.1-64bit-portable.zip ;
+7z.exe x .\perl.zip -xr!c -oc:\strawberry ;
+
+cd c:\ ; `
+Remove-Item C:\t -Force -Recurse
